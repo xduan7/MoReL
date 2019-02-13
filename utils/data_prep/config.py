f7df82a376d4680b2f47e016d04d563c27a397d6
xduan7 <xduan7@gmail.com>
2019-02-13 11:50:04 -0600

    Extracting and serializing molecule done. Need featurization.

--- utils/data_prep/config.py
+++ utils/data_prep/config.py
@@ -9,17 +9,16 @@
         This file saves all the constants that are related to data
         downloading, pre-processing and storing.
 """
-import os
+from os.path import abspath, join
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(os.path.realpath(__file__)))))
-DATA_DIR = os.path.join(PROJECT_DIR, 'data/')
-RAW_DATA_DIR = os.path.join(DATA_DIR, 'raw/')
-PROCESSED_DATA_DIR = os.path.join(DATA_DIR, 'processed/')
-CID_MOL_DATA_DIR = os.path.join(PROCESSED_DATA_DIR, 'CID-Mol/')
-CID_SMILES_DATA_DIR = os.path.join(PROCESSED_DATA_DIR, 'CID-SMILES/')
-CID_ECFP_DATA_DIR = os.path.join(PROCESSED_DATA_DIR, 'CID-ECFP/')
-CID_GRAPH_DATA_DIR = os.path.join(PROCESSED_DATA_DIR, 'CID-Graph/')
+PROJECT_DIR = abspath(join(abspath(__file__), '../../../'))
+DATA_DIR = join(PROJECT_DIR, 'data/')
+RAW_DATA_DIR = join(DATA_DIR, 'raw/')
+PROCESSED_DATA_DIR = join(DATA_DIR, 'processed/')
+CID_MOL_DATA_DIR = join(PROCESSED_DATA_DIR, 'CID-Mol/')
+CID_SMILES_DATA_DIR = join(PROCESSED_DATA_DIR, 'CID-SMILES/')
+CID_ECFP_DATA_DIR = join(PROCESSED_DATA_DIR, 'CID-ECFP/')
+CID_GRAPH_DATA_DIR = join(PROCESSED_DATA_DIR, 'CID-Graph/')
 
 
 PCBA_CID_FILE_NAME = 'Cid2BioactivityLink'
@@ -35,21 +34,27 @@ CID_INCHI_FTP_ADDRESS = \
 
 
 # CID in PCBA dataset, ranging from 2 to 135,693,611, total size ~3,400,000
-PCBA_CID_FILE_PATH = os.path.join(RAW_DATA_DIR, PCBA_CID_FILE_NAME)
-CID_INCHI_FILE_PATH = os.path.join(RAW_DATA_DIR, CID_INCHI_FILE_NAME)
+PCBA_CID_FILE_PATH = join(RAW_DATA_DIR, PCBA_CID_FILE_NAME)
+CID_INCHI_FILE_PATH = join(RAW_DATA_DIR, CID_INCHI_FILE_NAME)
 
 
-MAX_NUM_ATOM = 32
-MAX_SMILES_LEN = 128
+# Note that 'base64' is more spatially efficient than 'hex'
+MOL_BINARY_ENCODING = 'base64'
+
+
+# Requirements for the molecules
+MAX_NUM_ATOM = 64
+MAX_LEN_SMILES = 128
+MAX_LEN_MOL_STR = 1024
 
 RANDOM_STATE = 0
 VALIDATION_SIZE = 5000
 
 
 UNUSED_CID_FILE_NAME = 'unused_CID.txt'
-UNUSED_CID_FILE_PATH = os.path.join(PROCESSED_DATA_DIR, UNUSED_CID_FILE_NAME)
+UNUSED_CID_FILE_PATH = join(PROCESSED_DATA_DIR, UNUSED_CID_FILE_NAME)
 CID_MOL_FILE_NAME = 'CID-Mol.hdf5'
-CID_MOL_FILE_PATH = os.path.join(PROCESSED_DATA_DIR, CID_MOL_FILE_NAME)
+CID_MOL_FILE_PATH = join(PROCESSED_DATA_DIR, CID_MOL_FILE_NAME)
 
 # Need to specify the FP (probably using ECFP4(1024) + ECFP6(1024))
 # CID_ECFP_FILE_NAME
@@ -60,7 +65,7 @@ CID_MOL_FILE_PATH = os.path.join(PROCESSED_DATA_DIR, CID_MOL_FILE_NAME)
 # CID_GRAPH_FILE_NAME
 
 CHUNK_CID_FILE_NAME = './CID-Mol/chunk_num-CID.txt'
-CHUNK_CID_FILE_PATH = os.path.join(PROCESSED_DATA_DIR, CHUNK_CID_FILE_NAME)
+CHUNK_CID_FILE_PATH = join(PROCESSED_DATA_DIR, CHUNK_CID_FILE_NAME)
 
 
 
--- utils/data_prep/data_prep.py
+++ utils/data_prep/data_prep.py
@@ -7,8 +7,14 @@
     File Description:   
 
         Prepare training/validation data.
+
+        This file is pretty much the summation of mol_prep, smiles_prep,
+        ecfp_prep, and graph_prep.
 """
+import codecs
 import json
+import pickle
+
 import numpy as np
 import pandas as pd
 from rdkit import Chem
@@ -18,7 +24,19 @@ import utils.data_prep.config as c
 from utils.data_prep.download import download
 
 
-def data_prep():
+def mol_to_str(mol: Chem.Mol):
+    return codecs.encode(mol.ToBinary(), c.MOL_BINARY_ENCODING).decode()
+
+
+def str_to_mol(mol_str: str):
+    try:
+        return Chem.Mol(codecs.decode(codecs.encode(mol_str),
+                                      c.MOL_BINARY_ENCODING))
+    except:
+        return None
+
+
+def data_prep(pcba_only=True):
 
     # Download and unpack data into raw data fodler ###########################
     download()
@@ -58,38 +76,65 @@ def data_prep():
     # Save all the molecules as intermediate results
 
     unused_cid = []
-    cid_mol_list = []
 
-    for chunk_idx, cid_inchi_df_chunk in enumerate(
+    cid_mol_hdf5 = pd.HDFStore(c.CID_MOL_FILE_PATH)
+    min_itemsize = 0
+
+    for chunk_idx, chunk_cid_inchi_df in enumerate(
             pd.read_csv(c.CID_INCHI_FILE_PATH,
                         sep='\t',
                         header=None,
                         index_col=[0],
                         usecols=[0, 1],
                         chunksize=2 ** 12)):
+        chunk_cid_mol_list = []
 
-        for cid, row in cid_inchi_df_chunk.iterrows():
+        for cid, row in chunk_cid_inchi_df.iterrows():
 
+            # Skip this compound if it is not in PCBA
             if cid not in pcba_cid_set:
                 continue
 
             inchi = row[1]
             try:
-                mol = Chem.MolFromInchi(inchi)
+                mol: Chem.rdchem.Mol = Chem.MolFromInchi(inchi)
                 assert mol
+
+                num_atoms: int = mol.GetNumAtoms()
+                len_smiles: int = len(Chem.MolToSmiles(mol))
+
+                # Skip the molecules with too many atoms or lengthy SMILES
+                if num_atoms > c.MAX_NUM_ATOM or len_smiles > c.MAX_LEN_SMILES:
+                    continue
+
             except AssertionError:
                 # print('Failed converting compound (CID=%i) to Mol.' % cid)
                 unused_cid.append(cid)
                 continue
 
-            cid_mol_list.append([cid, mol])
+            # Serialize and encode the molecule object
+            mol_str = mol_to_str(mol)
+            min_itemsize = max(min_itemsize, len(mol_str))
+
+            if len(mol_str) > c.MAX_LEN_MOL_STR:
+                # print('Compund %s has %i number of characters after encoding.'
+                #       % (Chem.MolToSmiles(mol), len(enc_mol)))
+                unused_cid.append(cid)
+                continue
+
+            chunk_cid_mol_list.append([cid, mol_str])
+
+        chunk_cid_mol_df = pd.DataFrame(chunk_cid_mol_list,
+                                        columns=['CID', 'Mol'])
+        chunk_cid_mol_df.set_index('CID', inplace=True)
+        cid_mol_hdf5.append(key='RDkit Molecule',
+                            value=chunk_cid_mol_df,
+                            min_itemsize=c.MAX_LEN_MOL_STR)
 
     with open(c.UNUSED_CID_FILE_PATH, 'w') as f:
         json.dump(unused_cid, f, indent=4)
 
-    cid_mol_df = pd.DataFrame(cid_mol_list, columns=['CID', 'Mol'])
-    cid_mol_df.set_index('CID', inplace=True)
-    cid_mol_df.to_hdf(c.CID_MOL_FILE_PATH, key='RDkit Molecule')
+    print('The maximum length of encoded molecule is %i.' % min_itemsize)
 
     return
 
