5ac07cba8e22ed100ddd222f4f648e77716fed4a
xduan7 <xduan7@gmail.com>
2019-07-29 13:18:50 -0500

    Modified hyper-parameter execution file.

--- task/graph_drug_response.sh
+++ task/graph_drug_response.sh
@@ -1,1 +1,5 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 comet optimize -j 4 graph_drug_response.py graph_drug_response.config
+export COMET_OPTIMIZER_ID=$(comet optimize task/graph_drug_response.config)
+CUDA_VISIBLE_DEVICES=4 comet optimize graph_drug_response.py graph_drug_response.config &
+CUDA_VISIBLE_DEVICES=5 comet optimize graph_drug_response.py graph_drug_response.config &
+CUDA_VISIBLE_DEVICES=6 comet optimize graph_drug_response.py graph_drug_response.config &
+CUDA_VISIBLE_DEVICES=7 comet optimize graph_drug_response.py graph_drug_response.config &
